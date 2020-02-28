19900ddf1e3dfde82ff7183eb424597209d67a25
aluna1997 <aluna1997@ciencias.unam.mx>
2020-02-27 21:08:23 -0600

    Primer avance

---
+++ capitulo_1/01.js
@@ -0,0 +1,8 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Hola mundo con Vue',
+        nombres: ['Pedro','Juan','Pancho'],
+        autos:[{marca:'Audi',modelo:'KJH23'},{marca:'Tesla',modelo:'T3'}]
+    }
+})
