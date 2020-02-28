a4befeb40d0fec278eb29521ca31babddfdfa363
aluna1997 <aluna1997@ciencias.unam.mx>
2020-02-27 21:22:12 -0600

    Segundo avence

--- capitulo_1/01.js
+++ capitulo_1/01.js
@@ -1,8 +1,20 @@
 const app = new Vue({
+    //1
     el: '#app',
+    //2
     data: {
         titulo: 'Hola mundo con Vue',
         nombres: ['Pedro','Juan','Pancho'],
         autos:[{marca:'Audi',modelo:'KJH23'},{marca:'Tesla',modelo:'T3'}]
+    },
+    nuevo_auto: '',
+    //3
+    methods:{
+        agregarAuto(){
+            console.log("Click!")
+            this.autos.push({
+                marca: this.nuevo_auto, modelo: "Nuevo"
+            })
+        }
     }
 })
